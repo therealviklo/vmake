1b27d7677aa4885eb690596f0240c36b753ba2c5
viklo <viggo@gstromberg.nu>
2021-04-07 21:30:17 +0200

    version 2

--- vmake.cpp
+++ vmake.cpp
@@ -1,223 +1,204 @@
-#include <cstdlib>
+#include <cstdio>
+#include <filesystem>
 #include <string>
-#include <iostream>
-#include <sstream>
-#include <stdexcept>
+#include <vector>
+#include <cstdint>
 #include <thread>
 #include <chrono>
-#include <filesystem>
-#include <cstring>
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
-enum struct LanguageMode
-{
-	c,
-	cpp
-};
-
-bool endsWith(const std::string& str, const std::string& ending)
-{
-	auto c = str.rbegin();
-	for (auto ec = ending.rbegin(); ec != ending.rend(); ec++)
-	{
-		if (c == str.rend()) return false;
-
-		if (*c != *ec) return false;
-
-		c++;
-	}
-	return true;
-}
-
-void addFilenames(std::stringstream& ss, const std::string& dir, LanguageMode mode)
-{
-	using namespace std::string_literals;
-	for (const auto& f : std::filesystem::recursive_directory_iterator(dir))
-	{
-		if (!f.is_directory())
-		{
-			const auto& path = f.path();
-			const auto pathString = path.string();
-			if (pathString != "."s && pathString != ".."s)
-			{
-				if (endsWith(pathString, mode == LanguageMode::c ? ".c" : ".cpp"))
-				{
-					ss << " \"" << (dir / path) << "\"";
-				}
-			}
-		}
-	}
-}
+using namespace std::literals;
 
 int main(int argc, char* argv[])
 {
+#ifdef _WIN32
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+#endif
 	try
 	{
-#ifdef _WIN32
-		SetConsoleCP(CP_UTF8);
-		SetConsoleOutputCP(CP_UTF8);
-#endif
-		
-		LanguageMode mode = LanguageMode::cpp;
-		std::string token = "NAMN";
-		int bells = 0;
-		int bellInterval = 900;
-
-		int currArgument = 1;
-
-		// Hantera flaggor
-		for (; currArgument < argc && (argv[currArgument][0] == '/' || argv[currArgument][0] == '-'); currArgument++)
-		{
-			if (std::strlen(argv[currArgument]) != 2)
+		int arg = 1;
+		auto advanceArg = [&arg, &argc]() -> void {
+			if (++arg >= argc)
 			{
-				std::cerr << "Ogiltigt argument: " << argv[currArgument] << '\n';
-				return 1;
+				std::puts("Ogiltig användning av argument");
+				std::exit(EXIT_FAILURE);
 			}
+		};
 
-			switch (argv[currArgument][1])
+		bool extensionsChanged = false;
+		std::vector<std::string> extensions = {".cpp"};
+		std::string output;
+		std::string objectExt = ".o";
+		std::string objs;
+		std::uintmax_t bells = 0;
+		unsigned long long ms = 50;
+
+		for (; arg < argc; arg++)
+		{
+			if (argv[arg][0] != '-') break;
+			switch (argv[arg][1])
 			{
 				case 'h':
-				case 'H':
 				case '?':
 				{
-					std::cout
-					<< "Syntax:\n"
-					<< "    vmake [flaggor] kommando\n"
-					<< "Flaggor:\n"
-					<< "    -h, -?   Visa hjälp.\n"
-					<< "    -c       Leta efter .c-filer istället för .cpp-filer.\n"
-					<< "    -t TOKEN Byt ut TOKEN mot listan på de filer som har hittats.\n"
-					<< "             Om denna flagga utelämnas används \"NAMN\".\n"
-					<< "    -b       Spela ett ljud när kommandot har utförts.\n"
-					<< "             Om denna flagga anges flera gånger spelas ljudet\n"
-					<< "             flera gånger.\n"
-					<< "    -i MS    Om -b anges flera gånger vänter vmake MS\n"
-					<< "             millisekunder mellan varje ljud. Default är\n"
-					<< "             900 millisekunder."
-					<< '\n';
-					return 0;
+					std::printf(
+						"Syntax:\n"
+						"    vmake [flaggor] kompilator [kompilatorflaggor]\n"
+						"Flaggor:\n"
+						"    -h, -?      Visa hjälp.\n"
+						"    -e ÄNDELSE  Ange en filändelse som ska sökas\n"
+						"                efter. Default är \".cpp\".\n"
+						"    -o FILNAMN  Ange programmets namn. Default är\n"
+						"                kompilatorns default.\n"
+						"    -O ÄNDELSE  Ange objektfiländelsen. Default\n"
+						"                är \".o\".\n"
+						"    -x FILNAMN  Lägg till en extra objektfil som\n"
+						"                också ska länkas in i programmet.\n"
+						"    -b          Spela ett plingljud när\n"
+						"                kompileringen är klar.\n"
+						"    -i MS       Ange intervallet för plingningar\n"
+						"                från -b.\n"
+					);
 				}
-				break;
-				case 'c':
-				case 'C':
+				return EXIT_SUCCESS;
+				case 'e':
 				{
-					mode = LanguageMode::c;
-				}
-				break;
-				case 't':
-				case 'T':
-				{
-					currArgument++;
-					if (currArgument >= argc)
+					advanceArg();
+					if (!extensionsChanged)
 					{
-						std::cerr << "Felaktig användning av argument: " << argv[currArgument - 1] << '\n';
-						return 1;
+						extensionsChanged = true;
+						extensions.clear();
 					}
-					token = argv[currArgument];
+					extensions.emplace_back(argv[arg]);
+				}
+				break;
+				case 'o':
+				{
+					advanceArg();
+					output = argv[arg];
+				}
+				break;
+				case 'O':
+				{
+					advanceArg();
+					objectExt = argv[arg];
+				}
+				break;
+				case 'x':
+				{
+					advanceArg();
+					objs += ' ';
+					objs += argv[arg];
 				}
 				break;
 				case 'b':
-				case 'B':
 				{
 					bells++;
 				}
 				break;
 				case 'i':
-				case 'I':
 				{
-					currArgument++;
-					if (currArgument >= argc)
-					{
-						std::cerr << "Felaktig användning av argument: " << argv[currArgument - 1] << '\n';
-						return 1;
-					}
-
-					std::size_t charsRead = 0;
-					try
-					{
-						bellInterval = std::stoi(argv[currArgument], &charsRead);
-					}
-					catch (std::invalid_argument)
-					{
-						std::cerr << "Felaktig användning av argument: " << argv[currArgument - 1] << '\n';
-						std::cerr << "(Antalet millisekunder är inte ett giltigt heltal)" << '\n';
-						return 1;
-					}
-					catch (std::out_of_range)
-					{
-						std::cerr << "Felaktig användning av argument: " << argv[currArgument - 1] << '\n';
-						std::cerr << "(Antalet millisekunder får inte plats i en int)" << '\n';
-						return 1;
-					}
-
-					// Ser till att det inte finns text efter talet
-					if (charsRead != std::strlen(argv[currArgument]))
-					{
-						std::cerr << "Felaktig användning av argument: " << argv[currArgument - 1] << '\n';
-						std::cerr << "(Antalet millisekunder är inte ett giltigt heltal)" << '\n';
-						return 1;
-					}
-
-					if (bellInterval < 0)
-					{
-						std::cerr << "Felaktig användning av argument: " << argv[currArgument - 1] << '\n';
-						std::cerr << "(Antalet millisekunder är mindre än noll)" << '\n';
-						return 1;
-					}
+					advanceArg();
+					ms = std::stoull(argv[arg]);
 				}
 				break;
 				default:
 				{
-					std::cerr << "Ogiltigt argument: " << argv[currArgument] << '\n';
-					return 1;
+					std::printf("Okänt argument: %s\n", argv[arg]);
+				}
+				return EXIT_FAILURE;
+			}
+		}
+
+		if (arg >= argc)
+		{
+			std::puts("Ingen kompilator specificerades.");
+			return EXIT_FAILURE;
+		}
+		const std::string compiler = argv[arg++];
+
+		std::string compArgs;
+		for (; arg < argc; arg++)
+		{
+			compArgs += ' ';
+			compArgs += argv[arg];
+		}
+
+		std::vector<std::filesystem::directory_entry> files;
+		for (const auto& i : std::filesystem::recursive_directory_iterator("."))
+		{
+			const std::string ext = i.path().extension().string();
+			if (ext.size() > 1)
+			{
+				for (const auto& j : extensions)
+				{
+					if (j == ext)
+					{
+						files.emplace_back(i.path());
+					}
 				}
 			}
 		}
 
-		std::stringstream ss;
-		addFilenames(ss, std::filesystem::current_path().string(), mode);
+		for (size_t i = 0; i < files.size(); i++)
+		{
+			const std::string obji = files[i].path().stem().string() + objectExt;
+			objs += ' ';
+			objs += obji;
 
-		std::stringstream command;
-		if (currArgument < argc)
-		{
-			command << argv[currArgument] << " ";
-			currArgument++;
-		}
-		for (; currArgument < argc; currArgument++)
-		{
-			if (argv[currArgument] == token)
+			std::printf("(%zu/%zu) %s", i + 1, files.size(), files[i].path().string().c_str());
+			if (!std::filesystem::exists(obji) || files[i].last_write_time() > std::filesystem::last_write_time(obji))
 			{
-				command << ss.str() << " ";
+				std::putchar('\n');
+				std::system((
+					compiler +
+					" "s +
+					files[i].path().string() +
+					" -c -o "s +
+					obji +
+					compArgs
+				).c_str());
 			}
 			else
 			{
-				command << "\"" << argv[currArgument] << "\" ";
+				std::puts(" (Skippar)");
 			}
 		}
 
-		int returnValue = system(command.str().c_str());
+		std::puts("Länkar");
 
-		for (int i = 0; i < bells; i++)
+		std::system((
+			compiler +
+			objs +
+			(output.empty() ? ""s : " -o "s + output) +
+			compArgs
+		).c_str());
+
+		for (unsigned long long i = 0; i < bells; i++)
 		{
-			if (i > 0) std::this_thread::sleep_for(std::chrono::milliseconds(bellInterval));
+			if (i > 0)
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+			}
 #ifdef _WIN32
-			MessageBeep(MB_OK);
+			MessageBeep(MB_ICONINFORMATION);
 #else
-			std::cout << '\a';
+			std::putchar('\a');
 #endif
 		}
-
-		return returnValue;
 	}
 	catch (const std::exception& e)
 	{
-		std::cerr << "Fel: " << e.what() << '\n';
+		std::printf("Fel: %s\n", e.what());
+		return EXIT_FAILURE;
 	}
 	catch (...)
 	{
-		std::cerr << "Okänt fel" << '\n';
+		std::puts("Okänt fel");
+		return EXIT_FAILURE;
 	}
-	return 1;
+	return EXIT_SUCCESS;
 }
